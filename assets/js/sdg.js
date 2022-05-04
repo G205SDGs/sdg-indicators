9852f8c329a7debd1f2ff620dbc13ccee0af25e6
MoBosse <50657629+MoBosse@users.noreply.github.com>
2022-05-04 11:26:28 +0000

    Deploying to gh-pages from  @ 19232bc7cd7c15edb3b7c60cea0ca0b0487844b5 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1228,7 +1228,7 @@ function nonFieldColumns() {
     'Unit multiplier',
     'Unit measure',
   ];
-  var timeSeriesAttributes = null;
+  var timeSeriesAttributes = [{"field":"COMMENT_TS","label":"indicator.footnote"},{"field":"DATA_LAST_UPDATE","label":"metadata_fields.national_data_update_url"}];
   timeSeriesAttributes.forEach(function(tsAttribute) {
     columns.push(tsAttribute.field);
   });
@@ -2608,7 +2608,7 @@ function getTimeSeriesAttributes(rows) {
     return [];
   }
   var timeSeriesAttributes = [],
-      possibleAttributes = null,
+      possibleAttributes = [{"field":"COMMENT_TS","label":"indicator.footnote"},{"field":"DATA_LAST_UPDATE","label":"metadata_fields.national_data_update_url"}],
       firstRow = rows[0],
       firstRowKeys = Object.keys(firstRow);
   possibleAttributes.forEach(function(possibleAttribute) {
@@ -3489,7 +3489,7 @@ var indicatorView = function (model, options) {
   }
 
   this.updateTimeSeriesAttributes = function(tsAttributeValues) {
-    var timeSeriesAttributes = null;
+    var timeSeriesAttributes = [{"field":"COMMENT_TS","label":"indicator.footnote"},{"field":"DATA_LAST_UPDATE","label":"metadata_fields.national_data_update_url"}];
     timeSeriesAttributes.forEach(function(tsAttribute) {
       var field = tsAttribute.field,
           valueMatch = tsAttributeValues.find(function(tsAttributeValue) {
@@ -4750,7 +4750,7 @@ $(function() {
           '<span class="arrow right"></span>' +
         '</div>';
 
-
+      
       var swatchTpl = '<span class="legend-swatch" style="width:{width}%; background:{color};"></span>';
       var swatchWidth = 100 / this.plugin.options.colorRange[this.plugin.goalNr].length;
       var swatches = this.plugin.options.colorRange[this.plugin.goalNr].map(function(swatchColor) { //[this.plugin.goalNr]
